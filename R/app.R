9e08fe38201588b1c61e8809ff82295e53e478d1
Raoul Schorer <raoul.schorer@hug.ch>
2025-03-08 02:28:46 +0100

    added legend for row colors

--- R/app.R
+++ R/app.R
@@ -15,11 +15,11 @@ recovery.app <- function(data,...) {
     if(!is.data.frame(data)) stop("The data you provided is not a data.frame.")
     if("recovery.reserved.test.tf" %in% colnames(data)) stop("'recovery.reserved.test.tf' is a reserved column name")
     if("recovery.reserved.cond.tf" %in% colnames(data)) stop("'recovery.reserved.cond.tf' is a reserved column name")
-    if("recovery.reserved.test.cond" %in% colnames(data)) stop("'recovery.reserved.test.cond' is a reserved column name")
+    if("class" %in% colnames(data)) stop("'class' is a reserved column name")
 
     data$recovery.reserved.test.tf <- FALSE
     data$recovery.reserved.cond.tf <- FALSE
-    data$recovery.reserved.test.cond <- "NA|NA"
+    data$class <- "NA"
     
     ui <- shiny::fluidPage(
                      shiny::tags$script(shiny::HTML(js)),
@@ -44,9 +44,11 @@ recovery.app <- function(data,...) {
                                           recovery.reserved.test.tf <- eval(test.expr) #this NEEDS preprocessing !
                                           recovery.reserved.cond.tf <- eval(cond.expr)})
                                       within(test.cond,{
-                                          recovery.reserved.test.cond <- as.factor(paste0(recovery.reserved.test.tf,
-                                                                                          "|",
-                                                                                          recovery.reserved.cond.tf))})
+                                          class <- factor(paste0(recovery.reserved.test.tf,
+                                                                                       "|",
+                                                                                       recovery.reserved.cond.tf),
+                                                                                levels=c("TRUE|TRUE","TRUE|FALSE","FALSE|TRUE","FALSE|FALSE","NA|TRUE","NA|FALSE","FALSE|NA","TRUE|NA","NA|NA"),
+                                                                                labels=c("T+","F+","F-","T-",rep("NA",5)))})
                                   } else {
                                       data
                                   }},
@@ -54,17 +56,29 @@ recovery.app <- function(data,...) {
         output$data.table <- DT::renderDataTable(
                                      DT::formatStyle(
                                              DT::datatable(test.tf(),
+                                                           caption=shiny::tags$caption(
+                                                                                   style="caption-side: top; text-align: center;",
+                                                                                   "Row colors: ",
+                                                                                   shiny::em("True positive",
+                                                                                             shiny::span(style=paste("color:",palette.colors(palette="Set 2")[1]),"\U2b1b\Ufe0e")),
+                                                                                   shiny::em("False positive",
+                                                                                             shiny::span(style=paste("color:",palette.colors(palette="Set 2")[2]),"\U2b1b\Ufe0e")),
+                                                                                   shiny::em("False negative",
+                                                                                             shiny::span(style=paste("color:",palette.colors(palette="Set 2")[3]),"\U2b1b\Ufe0e")),
+                                                                                   shiny::em("True negative",
+                                                                                             shiny::span(style=paste("color:",palette.colors(palette="Set 2")[4]),"\U2b1b\Ufe0e")),
+                                                                                   shiny::em("Unknown/missing",
+                                                                                             shiny::span(style="color: black","\U2b1c\Ufe0e")),
+                                                                                   ),
                                                            options=list(
                                                                lengthMenu=list(c(10,25,50,-1),c("10","25","50","All")),
                                                                columnDefs=list(
                                                                    list(visible=FALSE,
                                                                         targets=c("recovery.reserved.test.tf",
-                                                                                  "recovery.reserved.cond.tf",
-                                                                                  "recovery.reserved.test.cond"))))),
-                                             "recovery.reserved.test.cond",
+                                                                                  "recovery.reserved.cond.tf"))))),
+                                             "class",
                                              target="row",
-                                             backgroundColor = DT::styleEqual(c("TRUE|TRUE","TRUE|FALSE","FALSE|TRUE","FALSE|FALSE",
-                                                                                "NA|TRUE","NA|FALSE","FALSE|NA","TRUE|NA","NA|NA"),
+                                             backgroundColor = DT::styleEqual(c("T+","F+","F-","T-",rep("NA",5)),
                                                                               c(palette.colors(palette="Set 2",n=4),
                                                                                 rep("white",5)))))
     }
